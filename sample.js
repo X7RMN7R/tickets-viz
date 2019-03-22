e6c5e27e5507cf5f35105a1921415ebf0d7132a7
yves_vilbe <yv@vif.fr>
2019-03-22 16:42:31 +0100

    Init project

---
+++ sample.js
@@ -0,0 +1,43 @@
+// This code sample uses the 'request' library:
+// https://www.npmjs.com/package/request
+var request = require('request');
+
+var jqlRequest = 'MES AND status in (Blocked, "In Progress", OPEN, Reopened, Reviewing, Tested, "Waiting for external release") AND Sous-projets = LP ORDER BY priority DESC, updated DESC';
+
+var bodyData = `{
+  "expand": [
+    "names",
+    "schema",
+    "operations"
+  ],
+  "jql": "project = MES AND status in (Blocked, 'In Progress', OPEN, Reopened, Reviewing, Tested, 'Waiting for external release')",
+  "maxResults": 15,
+  "fieldsByKeys": false,
+  "fields": [
+    "summary",
+    "status",
+    "assignee"
+  ],
+  "startAt": 0
+}`;
+
+var options = {
+    method: 'POST',
+    url: 'https://vif-hub.atlassian.net/rest/api/3/search',
+    auth: /* See .auth file */,
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json'
+    },
+    body: bodyData
+};
+
+
+
+request(options, function (error, response, body) {
+   if (error) throw new Error(error);
+   console.log(
+      'Response: ' + response.statusCode + ' ' + response.statusMessage
+   );
+   console.log(body);
+});
